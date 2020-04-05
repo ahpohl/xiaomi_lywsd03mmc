aac3c674497fd44207a112b6c4a4841d72143944
Alexander Pohl <alex@ahpohl.com>
2020-04-05 16:28:11 +0200

    parse packet complete

--- include/ble.hpp
+++ include/ble.hpp
@@ -11,11 +11,14 @@ public:
   void setDebug(void);
   void readPacketFile(char* t_file);
   void parsePacket(void) const;
+  std::string decryptPaylod(std::string const& cipher, std::string const& key,
+    std::string const& iv) const;
   
 private:
   bool m_debug;
   std::string m_packet;
   static const std::map<std::string, std::string> XIAOMI_TYPE;
+  static const std::map<std::string, std::string> XIAOMI_KEYS;
 };
 
 #endif // BLE_HPP
--- src/ble.cpp
+++ src/ble.cpp
@@ -19,6 +19,17 @@ const map<string, string> Ble::XIAOMI_TYPE = {
   {"\x76\x05", "CGD1"}  
 };
 
+const map<string, string> Ble::XIAOMI_KEYS = {
+  {"A4:C1:38:4E:16:78", "e9efaa6873f9f9c87a5e75a5f814801c"},
+  {"A4:C1:38:BC:B9:B2", "66c0f070f7394bb753e11198e3061830"},
+  {"A4:C1:38:8C:34:B7", "cfc7cc892f4e32f7a733086cf3443cb0"},
+  {"A4:C1:38:B1:CD:7F", "eef418daf699a0c188f3bfd17e4565d9"},
+  {"A4:C1:38:BF:54:5D", "dc06a798095b178767c0b74185275352"},
+  {"A4:C1:38:80:C5:75", "a1b0dbe389e0d37d0cd569a81efc555f"},
+  {"A4:C1:38:8D:D3:19", "48403ebe2d385db8d0c187f81e62cb64"},
+  {"A4:C1:38:6A:11:C1", "317643d6c4e31929a7a4f833bde9520a"}
+};
+
 Ble::Ble(void)
 {
   m_debug = false;
@@ -89,8 +100,8 @@ void Ble::parsePacket(void) const
   if (rssi > 0 || rssi < -127) {
     throw runtime_error("Invalid RSSI signal strength");
   }
-  // get sensor type from dictionary, report unknown
-  string sensor_type("Hello World");
+  // check sensor type, report unknown
+  string sensor_type;
   try {
     sensor_type = XIAOMI_TYPE.at(m_packet.substr(pos+5, 2));
   }
@@ -104,4 +115,54 @@ void Ble::parsePacket(void) const
       << " dBm, MAC: " << encoded << endl;
     throw runtime_error(e.what());
   }
+  // check frame control flags, big endian format
+  uint16_t framectrl = (m_packet[pos+3] << 8) | (m_packet[pos+4] & 0xFF);
+  if (!(framectrl & 0x4000)) {
+    throw runtime_error("No ADV payload present");
+  }
+  size_t payload_length;
+  payload_length = (framectrl & 0x2000) ? m_packet.length()-pos-16 : 
+    m_packet.length()-pos-15;
+  if (payload_length < 3) {
+    throw runtime_error("Invalid ADV payload length");
+  }
+  size_t payload_pos;
+  payload_pos = (framectrl & 0x2000) ? pos+15 : pos+14;
+  if (payload_length != (m_packet.substr(payload_pos+1, string::npos).length()))  {
+    throw runtime_error("Invalid ADV payload start");
+  }
+  string cipher = m_packet.substr(payload_pos, payload_length);
+  // check encrypted data flags
+  if (!(framectrl & 0x0800)) {
+    throw runtime_error("Plaintext ADV payload");
+  }
+  // lookup encryption key
+  string enc_mac;
+  reverse(mac_xiaomi.begin(), mac_xiaomi.end());
+  CryptoPP::StringSource ssm(mac_xiaomi, true, new CryptoPP::HexEncoder(
+    new CryptoPP::StringSink(enc_mac), true, 2, ":")
+  );
+  string enc_key;
+  try {
+    enc_key = XIAOMI_KEYS.at(enc_mac);
+  }
+  catch (exception const& e) {
+    cout << "MAC address " << enc_mac << ": encryption key unknown" << endl;
+    throw runtime_error(e.what());
+  }
+  string key;
+  CryptoPP::StringSource ssk(enc_key, true, new CryptoPP::HexDecoder(
+    new CryptoPP::StringSink(key)));
+  string packet_id = m_packet[pos+7];
+  string iv = mac_source + m_packet.substr(pos+5, 2) + m_packet[pos+7];
+  string plaintext = decryptPayload(cipher, key, iv);
+}
+
+string Ble::decryptPaylod(string const& cipher, string const& key, 
+  string const& iv) const
+{
+  string plaintext;
+
+
+  return plaintext;
 }
