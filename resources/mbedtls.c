02fb33f275ecf6d672264e1c40fb001e003c9236
Alexander Pohl <alex@ahpohl.com>
2020-04-10 09:37:21 +0200

    add crypto++ test code, verify test vectors

--- resources/mbedtls.c
+++ resources/mbedtls.c
@@ -10,12 +10,12 @@
 
 typedef struct TestVector {
   char const* name;
-  uint8_t key[32];
-  uint8_t plaintext[MAX_PLAINTEXT_LEN];
-  uint8_t ciphertext[MAX_PLAINTEXT_LEN];
-  uint8_t authdata[20];
-  uint8_t iv[12];
-  uint8_t tag[16];
+  unsigned char key[32];
+  unsigned char plaintext[MAX_PLAINTEXT_LEN];
+  unsigned char ciphertext[MAX_PLAINTEXT_LEN];
+  unsigned char authdata[MAX_PLAINTEXT_LEN];
+  unsigned char iv[12];
+  unsigned char tag[16];
   size_t authsize;
   size_t datasize;
   size_t tagsize;
@@ -63,9 +63,34 @@ static TestVector const testVectorCCM = {
 };
 */
 
+// mbedtls/tests/suites/test_suite_cipher.ccm.data
+/*
+static TestVector const testVectorCCM = {
+    .name        = "AES-128 CCM NIST #25",
+    .key         = {0xf9, 0xfd, 0xca, 0x4a, 0xc6, 0x4f, 0xe7, 0xf0,
+    		        0x14, 0xde, 0x0f, 0x43, 0x03, 0x9c, 0x75, 0x71},
+    .plaintext   = {0xa2, 0x65, 0x48, 0x0c, 0xa8, 0x8d, 0x5f, 0x53,
+    		        0x6d, 0xb0, 0xdc, 0x6a, 0xbc, 0x40, 0xfa, 0xf0,
+			        0xd0, 0x5b, 0xe7, 0xa9, 0x66, 0x97, 0x77, 0x68},
+	.ciphertext  = {0x6b, 0xe3, 0x18, 0x60, 0xca, 0x27, 0x1e, 0xf4,
+				    0x48, 0xde, 0x8f, 0x8d, 0x8b, 0x39, 0x34, 0x6d,
+					0xaf, 0x4b, 0x81, 0xd7, 0xe9, 0x2d, 0x65, 0xb3},
+	.authdata    = {0x37, 0x96, 0xcf, 0x51, 0xb8, 0x72, 0x66, 0x52,
+    		        0xa4, 0x20, 0x47, 0x33, 0xb8, 0xfb, 0xb0, 0x47,
+					0xcf, 0x00, 0xfb, 0x91, 0xa9, 0x83, 0x7e, 0x22,
+					0xec, 0x22, 0xb1, 0xa2, 0x68, 0xf8, 0x8e, 0x2c},
+    .iv          = {0x5a, 0x8a, 0xa4, 0x85, 0xc3, 0x16, 0xe9},
+    .tag         = {0x38, 0xf1, 0x25, 0xfa},
+    .authsize    = 32,
+    .datasize    = 24,
+    .tagsize     = 4,
+    .ivsize      = 7
+};
+*/
+
 char* as_hex(unsigned char const* a, size_t a_size)
 {
-    char* s = malloc(a_size * 2 + 1);
+    unsigned char* s = (char*) malloc(a_size * 2 + 1);
     for (size_t i = 0; i < a_size; i++) {
         sprintf(s + i * 2, "%02X", a[i]);
     }
@@ -75,7 +100,7 @@ char* as_hex(unsigned char const* a, size_t a_size)
 int main(int argc, char* argv[])
 {
   int ret = 0;
-  uint8_t plaintext[MAX_PLAINTEXT_LEN];
+  unsigned char plaintext[MAX_PLAINTEXT_LEN] = {0};
 
   ret = mbedtls_ccm_self_test(1);
   if (ret) {
@@ -123,16 +148,18 @@ int main(int argc, char* argv[])
     testVectorCCM.tag,
     testVectorCCM.tagsize 
   );
+
+  printf("\n");
   if (ret) {
     if (ret == MBEDTLS_ERR_CCM_AUTH_FAILED) {
-      fprintf(stderr, "Authenticated decryption failed.\n");
+      fprintf(stderr, "MbedTLS    : Authenticated decryption failed.\n");
     } else if (ret == MBEDTLS_ERR_CCM_BAD_INPUT) {
-      fprintf(stderr, "Bad input parameters to the function.\n");
+      fprintf(stderr, "MbedTLS    : Bad input parameters to the function.\n");
     } else if (ret == MBEDTLS_ERR_CCM_HW_ACCEL_FAILED) {
-      fprintf(stderr, "CCM hardware accelerator failed.\n");
+      fprintf(stderr, "MbedTLS    : CCM hardware accelerator failed.\n");
     }
   } else {
-    printf("Decryption successful\n");
+    printf("MbedTLS    : Decryption successful\n");
   }
 
   encoded = as_hex(plaintext, testVectorCCM.datasize);
@@ -140,6 +167,8 @@ int main(int argc, char* argv[])
   free(encoded);
 
   mbedtls_ccm_free(&ctx);
+
+  unsigned char tag[testVectorCCM.tagsize] = testVectorCCM.ciphertext + testVectorCCM.datasize;
 
   return 0;
 }
